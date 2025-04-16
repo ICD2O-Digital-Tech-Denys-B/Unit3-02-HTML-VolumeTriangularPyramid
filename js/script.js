188dbbc38e3ea567a7b032387052e7fd78d56992
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-04-16 15:01:53 +0000

    done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+    function calcVol() {
+        let sideA = parseFloat(document.getElementById("side-a").value);
+        let sideB = parseFloat(document.getElementById("side-b").value);
+        let height = parseFloat(document.getElementById("height").value);
+        let step1 = sideA * sideB * height
+        let step2 = 1/6
+        let volume = step1*step2
+        document.getElementById("answer").innerHTML = "Answer:" + volume +" cm^3";
+}
